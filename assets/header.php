982aba74de891860ef8b04c7459ad4ba8a949673
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-10-13 21:17:06 +0200

    Creato il file magico header

---
+++ assets/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Foundation</title>
+  <!-- META TAG -->
+  <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=yes">
+  <meta name="description" content="Il sito ufficiale di Foundation, una comunità multigaming basata su Discord ed SL" />
+  <meta name="keywords" content="Foundation, SCP Foundation, SCP Foundation ITA, SCP:SL, SCP Secret Laboratory, SCP SL Foundation" />
+  <meta name="robots" content="all" />
+  <meta name="revisit-after" content="2" />
+  <meta name="language" content="it" />
+  <meta name="charset" content="utf-8" />
+  <meta name="copyright" content="FoxWorn3365" />
+  <meta name="author" content="FoxWorn3365" />
+  <meta name="reply-to" content="fox@scpfoundation.it" />
+  <!-- IMPORTO GLI STILI -->
+  <link rel="stylesheet" href="https://resources.fcosma.it/w3.css"> 
+  <link rel="stylesheet" href="https://resources.fcosma.it/fa/css/all.min.css"> 
+  <link rel="stylesheet" href="https://resources.fcosma.it/fonts/special/oswald.css">
+  <link rel="stylesheet" href="/assets/css/main.css">
+ </head>
+ <body id='body' class='foundation-main fox-oswald'>
+  <div id='head' class='w3-display-container foundation-head'>
+   <div id='presentation'>
+    <div id='presentation_video_dir'>
+     <video muted autoplay id='presentation_video' src='/assets/video/main.mp4' class='foundation-main-presentation'></video>
+    </div>
+    <div id='presentation_img_div'>
+     <img id='presentation_image' class='foundation-main-presentation'>
+    </div>
+   </div>
+   <div class="w3-bar w3-text-white foundation-menu">
+    <a href="#" class="w3-bar-item w3-button">Home</a>
+    <a href="/#community" class="w3-bar-item w3-button">Comunità</a>
+   </div> 
+   <p class='foundation-title w3-display-middle'>Foundation</p>
+  </div>
